083b16a17693d9bb479641dc028bcf662faebfed
crn1 <gluvajic@aol.com>
2019-09-29 13:50:30 +0200

    Updates

---
+++ precache-manifest.fac29660860c1d351d0e57761fbd780b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234b6cf22a371da48988a897628ddca0",
+    "url": "/andromeda-frontend/index.html"
+  },
+  {
+    "revision": "426910c456b683caa04b",
+    "url": "/andromeda-frontend/static/css/2.4b3ccb6c.chunk.css"
+  },
+  {
+    "revision": "426910c456b683caa04b",
+    "url": "/andromeda-frontend/static/js/2.cec2132f.chunk.js"
+  },
+  {
+    "revision": "30d5bd79866611081da4",
+    "url": "/andromeda-frontend/static/js/main.74b342b5.chunk.js"
+  },
+  {
+    "revision": "86348fe46cf53ae9ec0a",
+    "url": "/andromeda-frontend/static/js/runtime~main.62f503cc.js"
+  },
+  {
+    "revision": "ee6a3a588c0ec9eee4381b6f822c1ce4",
+    "url": "/andromeda-frontend/static/media/picture1.ee6a3a58.jpg"
+  },
+  {
+    "revision": "fb215782d5bfc3a0e7a92012ce6d21e3",
+    "url": "/andromeda-frontend/static/media/picture10.fb215782.jpg"
+  },
+  {
+    "revision": "55720bd6aef86d8ca8028235169870be",
+    "url": "/andromeda-frontend/static/media/picture2.55720bd6.jpg"
+  },
+  {
+    "revision": "c3b50d56dc2844a93366bc06eb7b45c3",
+    "url": "/andromeda-frontend/static/media/picture3.c3b50d56.jpg"
+  },
+  {
+    "revision": "9f52efb7088e94ebe73750c84a33f5b9",
+    "url": "/andromeda-frontend/static/media/picture4.9f52efb7.jpg"
+  },
+  {
+    "revision": "6c837d662028e3cc28248e4767af2ed1",
+    "url": "/andromeda-frontend/static/media/picture5.6c837d66.jpg"
+  },
+  {
+    "revision": "4379f2a8ca5ad7ed697178e53209a39b",
+    "url": "/andromeda-frontend/static/media/picture6.4379f2a8.jpg"
+  },
+  {
+    "revision": "363ffe663c6663491add372fad246ce6",
+    "url": "/andromeda-frontend/static/media/picture7.363ffe66.jpg"
+  },
+  {
+    "revision": "60d49551534441e396f9f12392bdc398",
+    "url": "/andromeda-frontend/static/media/picture8.60d49551.jpg"
+  },
+  {
+    "revision": "3c89df6591bf03ca2de6eb42e8bf3283",
+    "url": "/andromeda-frontend/static/media/picture9.3c89df65.jpg"
+  }
+]);
